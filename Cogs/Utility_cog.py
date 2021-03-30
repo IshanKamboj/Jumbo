48ca8583b71df23f54cbe1d2decf5862258ea8a9
Ishan Kamboj <themonkeycoder@TheMonkeyCoder.themonkeycoder.org>
2021-03-30 14:12:44 +0530

    fixed some bugs

--- Cogs/Utility_cog.py
+++ Cogs/Utility_cog.py
@@ -444,6 +444,6 @@ class Utility(commands.Cog):
             x = wikipedia.summary(query,sentences=7)
             pg = wikipedia.page(query)
             link = pg.url
-            await ctx.send(f"**{y}**\n More info can be found here: {link}")
+            await ctx.send(f"**{x}**\n More info can be found here: {link}")
         except Exception as e:
             print(str(e))
