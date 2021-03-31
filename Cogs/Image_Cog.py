1f8b1387537cbd894f3fe9ab0ba00cf466805907
Ishan Kamboj <themonkeycoder@TheMonkeyCoder.themonkeycoder.org>
2021-03-31 16:34:48 +0530

    command wanted added

---
+++ Cogs/Image_Cog.py
@@ -0,0 +1,29 @@
+import discord 
+from discord.ext import commands
+import PIL
+from PIL import Image, ImageDraw, ImageFont
+from .Listeners import AllListeners
+from io import BytesIO
+class ImageCommands(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.command(name="wanted")
+    @commands.check(AllListeners.check_enabled)
+    async def _wanted(self,ctx, user:discord.Member=None):
+        if user is None:
+            user = ctx.author
+        wanted = Image.open('wanted.jpg')
+        width, height = wanted.size
+        asset = user.avatar_url
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((300,300))
+        d = ImageDraw.Draw(wanted)
+        font = ImageFont.truetype(font='wanted.otf',size=35)
+        w,h = d.textsize(user.name,font=font)
+        posX = (width-w)/2
+        d.text((posX,500),user.name,fill=(0,0,0),font=font)
+        wanted.paste(pfp, (100,180))
+        wanted.save('wanted_img.jpg')
+        await ctx.send(file=discord.File('wanted_img.jpg'))
