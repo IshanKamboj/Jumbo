b82d4266d3253119679b332c6bc530a7ea80b591
Ishan Kamboj <themonkeycoder@TheMonkeyCoder.themonkeycoder.org>
2021-04-08 20:46:15 +0530

    fixed small bug in emoji sending

--- Cogs/Listeners.py
+++ Cogs/Listeners.py
@@ -106,14 +106,14 @@ class AllListeners(commands.Cog):
                     emoji_name = message.content[1:-1]
                     for emoji in message.guild.emojis:
                         if emoji_name == emoji.name:
-                            # webhooks = await message.channel.webhooks()
-                            # webhook = discord.utils.get(webhooks, name = "Imposter NQN")
-                            # if webhook is None:
-                            #     webhook = await message.channel.create_webhook(name = "Imposter NQN")
+                            webhooks = await message.channel.webhooks()
+                            webhook = discord.utils.get(webhooks, name = "Imposter NQN")
+                            if webhook is None:
+                                webhook = await message.channel.create_webhook(name = "Imposter NQN")
 
-                            # await webhook.send(str(emoji), username = message.author.name, avatar_url = message.author.avatar_url)
-                            await message.delete()
-                            await message.channel.send(str(emoji))
+                            await webhook.send(str(emoji), username = message.author.name, avatar_url = message.author.avatar_url)
+                            # await message.delete()
+                            # await message.channel.send(str(emoji))
                             await message.delete()
                             # break
             except Exception as e:
