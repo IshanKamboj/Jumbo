e0f4b2a2ced73820d8a6e1c4b408e13ba98f86ef
Ishan Kamboj <themonkeycoder@TheMonkeyCoder.themonkeycoder.org>
2021-04-12 10:33:47 +0530

    firebase rules chnged

--- Database/db_files.py
+++ Database/db_files.py
@@ -6,6 +6,18 @@ ConfigVars = {
     'authDomain':os.getenv('authDomain'),
     'projectId': os.getenv('projectId'),
     'storageBucket':os.getenv('storageBucket'),
+    'serviceAccount':{
+        "type":os.getenv("type"),
+        "project_id":os.getenv("projectId"),
+        "private_key_id":os.getenv("private_key_id"),
+        "private_key":os.getenv("private_key"),
+        "client_email":os.getenv("client_email"),
+        "client_id":os.getenv("client_id"),
+        "auth_uri":os.getenv("auth_uri"),
+        "token_uri":os.getenv("token_uri"),
+        "auth_provider_x509_cert_url":os.getenv("auth_provider_x509_cert_url"),
+        "client_x509_cert_url":os.getenv("client_x509_cert_url")
+    },
     'messagingSenderId':os.getenv('messagingSenderId'),
     'appId':os.getenv('appId'),
     'measurementId':os.getenv('measurementId'),
